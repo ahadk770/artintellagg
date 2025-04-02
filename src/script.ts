1ee4a57ea4a8d78c5f32101add2ce459e598ace7
Ahad Karedia <ahad.karedia@hudl.com>
2025-04-01 23:49:52 -0500

    1 shot everything

--- src/script.ts
+++ src/script.ts
@@ -1,2 +1,85 @@
-const msg: string = "Hello World";
-console.log(msg);
+import {
+  clearContent,
+  copyToClipboard,
+  debounce,
+  fillFromLocalStorage,
+  saveToLocalStorage,
+} from "./storageHelper.js";
+
+interface AIOption {
+  company: string;
+  website: string;
+  icon: string;
+}
+
+const ai_agg: AIOption[] = [
+  { company: "xAI", website: "https://grok.com/", icon: "./public/grok.svg" },
+  {
+    company: "Open AI",
+    website: "https://chatgpt.com/",
+    icon: "./public/openai.svg",
+  },
+  {
+    company: "Anthropic",
+    website: "https://claude.ai/new",
+    icon: "./public/claude.svg",
+  },
+  {
+    company: "Google Gemini",
+    website: "https://gemini.google.com/",
+    icon: "./public/gemini.svg",
+  },
+];
+
+const renderOption = (option: AIOption) => {
+  const optionContainer = document.createElement("div");
+  optionContainer.className = "optionContainer";
+
+  const img = document.createElement("img");
+  img.src = option.icon;
+  optionContainer.appendChild(img);
+
+  const detailsContainer = document.createElement("div");
+  detailsContainer.className = "detailsContainer";
+  const title = document.createElement("p");
+  title.textContent = option.company;
+  detailsContainer.appendChild(title);
+
+  const url = document.createElement("p");
+  url.textContent = option.website;
+  detailsContainer.appendChild(url);
+
+  optionContainer.appendChild(detailsContainer);
+
+  optionContainer.addEventListener("click", () => {
+    window.open(option.website, "_blank");
+  });
+
+  document.getElementById("options")?.appendChild(optionContainer);
+};
+
+const addEventListenerToRandomButton = () => {
+  document.getElementById("randomButton")?.addEventListener("click", () => {
+    const randomIdx = Math.floor(Math.random() * ai_agg.length);
+    window.open(ai_agg[randomIdx].website, "_blank");
+  });
+};
+
+ai_agg.forEach((a) => renderOption(a));
+addEventListenerToRandomButton();
+
+const setupPromptTextBox = () => {
+  document
+    .getElementById("savedPrompt")
+    ?.addEventListener("input", debounce(saveToLocalStorage, 500));
+
+  window.addEventListener("load", fillFromLocalStorage);
+
+  document
+    .getElementById("savedPromptToClipboard")
+    ?.addEventListener("click", copyToClipboard);
+  document
+    .getElementById("clearSavedPrompt")
+    ?.addEventListener("click", clearContent);
+};
+setupPromptTextBox();
---
+++ src/storageHelper.ts
@@ -0,0 +1,48 @@
+function debounce(func: () => void, delay: number) {
+  let timeout: number;
+  return function () {
+    clearTimeout(timeout);
+    timeout = setTimeout(func, delay);
+  };
+}
+
+function saveToLocalStorage() {
+  const savedPrompt = document.getElementById(
+    "savedPrompt"
+  ) as HTMLTextAreaElement;
+  localStorage.setItem("savedPromptContent", savedPrompt.value || "");
+}
+
+function fillFromLocalStorage() {
+  const savedPrompt = document.getElementById(
+    "savedPrompt"
+  ) as HTMLTextAreaElement;
+  const savedContent = localStorage.getItem("savedPromptContent");
+  if (savedContent && savedPrompt) {
+    savedPrompt.value = savedContent;
+  }
+}
+
+function copyToClipboard() {
+  const savedPrompt = document.getElementById("savedPrompt");
+  if (savedPrompt && savedPrompt instanceof HTMLTextAreaElement) {
+    savedPrompt.select();
+    document.execCommand("copy");
+  }
+}
+
+function clearContent() {
+  const savedPrompt = document.getElementById("savedPrompt");
+  if (savedPrompt && savedPrompt instanceof HTMLTextAreaElement) {
+    savedPrompt.value = "";
+    localStorage.removeItem("savedPromptContent");
+  }
+}
+
+export {
+  debounce,
+  saveToLocalStorage,
+  fillFromLocalStorage,
+  copyToClipboard,
+  clearContent,
+};
